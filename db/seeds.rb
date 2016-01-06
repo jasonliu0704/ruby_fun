65416d712d23bb565992de06a286f814a700ef09
Jiacheng Liu <jasonliu0704@gmail.com>
2016-01-05 23:57:11 -0800

    add account activation

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,7 +9,9 @@
 User.create!(name:  "Example User",
              email: "example@railstutorial.org",
              password:              "foobar",
-             password_confirmation: "foobar")
+             password_confirmation: "foobar",
+             activated: true,
+             activated_at: Time.zone.now)
 
 #create admin user
 User.create!(name:  "admin",
@@ -25,5 +27,7 @@ User.create!(name:  "admin",
   User.create!(name:  name,
                email: email,
                password:              password,
-               password_confirmation: password)
+               password_confirmation: password,
+               activated: true,
+               activated_at: Time.zone.now)
 end
--- test/integration/users_signup_test.rb
+++ test/integration/users_signup_test.rb
@@ -7,6 +7,7 @@ class UsersSignupTest < ActionDispatch::IntegrationTest
 
   def setup
     @user = users(:michael)
+    ActionMailer::Base.deliveries.clear
   end
 
   test "invalid signup information" do
@@ -30,9 +31,9 @@ class UsersSignupTest < ActionDispatch::IntegrationTest
                                             password:              "password",
                                             password_confirmation: "password" }
     end
-    assert_template 'users/show'
-    assert_not flash.empty?
-    assert is_logged_in?
+    #assert_template 'users/show'
+    #assert_not flash.empty?
+    #assert is_logged_in?
   end
 
   test "login with valid information followed by logout" do
@@ -52,5 +53,33 @@ class UsersSignupTest < ActionDispatch::IntegrationTest
     assert_select "a[href=?]", login_path
     assert_select "a[href=?]", logout_path,      count: 0
     assert_select "a[href=?]", user_path(@user), count: 0
+  end
+
+  test "valid signup information with account activation" do
+    get signup_path
+    assert_difference 'User.count', 1 do
+      post users_path, user: { name:  "Example User",
+                               email: "user@example.com",
+                               password:              "password",
+                               password_confirmation: "password" }
+    end
+    assert_equal 1, ActionMailer::Base.deliveries.size
+    user = assigns(:user)
+    assert_not user.activated?
+    # Try to log in before activation.
+    log_in_as(user)
+    assert_not is_logged_in?
+    # Invalid activation token
+    get edit_account_activation_path("invalid token")
+    assert_not is_logged_in?
+    # Valid token, wrong email
+    get edit_account_activation_path(user.activation_token, email: 'wrong')
+    assert_not is_logged_in?
+    # Valid activation token
+    get edit_account_activation_path(user.activation_token, email: user.email)
+    assert user.reload.activated?
+    follow_redirect!
+    assert_template 'users/show'
+    assert is_logged_in?
   end
 end
