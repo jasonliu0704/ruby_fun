99d80dca34ad296cec1621800fb7fe10cefb04ea
Jiacheng Liu <jasonliu0704@gmail.com>
2016-01-10 21:19:30 -0800

    fix

--- app/models/user.rb
+++ app/models/user.rb
@@ -67,9 +67,9 @@ class User < ActiveRecord::Base
 	end
 
 	# Returns true if a password reset has expired.
-  def password_reset_expired?
-    reset_sent_at < 2.hours.ago
-  end
+	 def password_reset_expired?
+	   reset_sent_at < 2.hours.ago
+	 end
 
 
 	private
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -81,7 +81,7 @@ Rails.application.configure do
 
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.delivery_method = :smtp
-  host = '<your heroku app>.herokuapp.com'
+  host = 'hidden-shore-6616.herokuapp.com'
   config.action_mailer.default_url_options = { host: host }
   ActionMailer::Base.smtp_settings = {
     :address        => 'smtp.sendgrid.net',
@@ -92,5 +92,5 @@ Rails.application.configure do
     :domain         => 'heroku.com',
     :enable_starttls_auto => true
   }
-  
+
 end
