0cdef0d133897e9c47d7fe781634966e4a1230ea
Jiacheng Liu <jasonliu0704@gmail.com>
2016-01-18 05:30:44 -0800

    add user subscription

--- config/routes.rb
+++ config/routes.rb
@@ -11,7 +11,11 @@ Rails.application.routes.draw do
   get 'contact' => 'static_pages#contact'
 
   #user resources
-  resources :users
+  resources :users do
+    member do
+      get :following, :followers
+    end
+  end
   get 'signup'  => 'users#new'
 
   #user login/out
@@ -23,6 +27,9 @@ Rails.application.routes.draw do
 
   resources :password_resets,     only: [:new, :create, :edit, :update]
   resources :microposts,          only: [:create, :destroy]
+  resources :relationships,       only: [:create, :destroy]
+
+
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
